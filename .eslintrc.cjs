03097cf0653e3d38862196e188a78a70c91c48c1
EungBug <eungb14@gmail.com>
2023-07-06 17:41:56 +0900

    docs: File Tree Setting, eslint rull add

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,11 @@ module.exports = {
     es2020: true,
     node: true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended'
+  ],
   overrides: [
     {
       env: {
@@ -25,6 +29,10 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 'warn',
     'no-extra-semi': 'off',
-    '@typescript-eslint/no-extra-semi': 'error'
+    '@typescript-eslint/no-extra-semi': 'error',
+    'no-new-object': 'warn', // 객체 선언 시 리터럴 방식으로 할 것
+    'array-bracket-newline': ['warn', 'always'], // 괄호 줄바꿈 처리 검사
+    'no-new-func': 'warn', // 함수 생성자 선언 X
+    'arrow-parens': 'always' // 화살표 함수 파라미터 1개인 경우 () 생략
   }
-};
+}
