b9a82956f73299f8b1f6dac0ead3dbf2657c37de
EungBug <eungb14@gmail.com>
2023-07-06 14:27:54 +0900

    docs: project package settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,30 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-  },
-}
+    'no-extra-semi': 'off',
+    '@typescript-eslint/no-extra-semi': 'error'
+  }
+};
