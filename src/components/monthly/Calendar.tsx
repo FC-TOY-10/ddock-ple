927e81127f4f728b47276ae65d198c820bb3aaf6
hahahaday12 <hahahaday12@naver.com>
2023-07-13 23:29:28 +0900

    feat: 리스트 수정, 삭제 기능추가

--- src/components/monthly/Calendar.tsx
+++ src/components/monthly/Calendar.tsx
@@ -3,21 +3,13 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; 
 import styled from 'styled-components';
 import { AiFillPlusCircle } from 'react-icons/ai'
-import { BsTrash } from "react-icons/bs";
+import { BsTrash, BsPencil, BsCheckSquare } from "react-icons/bs";
 import interactionPlugin from '@fullcalendar/interaction';
 import { ModalContent } from "./InputModal";
 import axios from "axios";
-import { error } from "console";
 
-type Data = {
-  amount: string;
-  _id: string;
-  date: Date;
-  title:string;
-  number: string;
-};
-
-type TResult = {
+interface TResult {
+  _id : string;
   title: number;
   date: string;
 };
@@ -30,47 +22,51 @@ interface Transaction {
   date: string;
 }
 
-type ViewDetailType = {
+interface ViewDetailType  {
   _id : string;
   amount : number;
   date: Date;
 }
 
-
 export default function Full () {
+  const [selectDate, setSelectDate] = useState(new Date());
   const [showModal, setShowModal] = useState(false);
+  const [viewDetail, setViewDetail] = useState<ViewDetailType[]>([]);
   const [viewData, setViewData] = useState<any>([{
-     _id:""
+    _id:""
     ,title:""
     ,date: new Date()
     ,amount:""
     , number:""
   }]);
-
-  const [viewDetail, setViewDetail] = useState<ViewDetailType[]>([]);
-
+  
   const [viewDrow, setViewDrow] = useState<TResult[]>([{
-   title:0
-   ,date: ""
+    _id:""
+    ,title:0
+    ,date: ""
   }]);
+  
+  // input 입력 상태 변경
+  const [editedTitle, setEditedTitle] = useState<string>('');
+  const [editingItemId, setEditingItemId] = useState<string>('');
+
 
   const Search = (params?:any) => { 
-    let sysdatetime = null;
+    console.log(params);
+    let sysdatetime:any = null;
+    console.log(params)
     if(params){
-      sysdatetime = params;
+      sysdatetime = params
     }else{
       sysdatetime = new Date();
     }
     const year = sysdatetime.getFullYear();
     const month = sysdatetime.getMonth() + 1;
-    console.log(month)
     getData(year,month)
     .then((response) => {
-      console.log(response?.data)
-      // setViewData(response);
       setViewData([response?.data]);
       setViewDrow(transformData(response?.data));
-      handleDateClick(null);
+      handleDateClick(sysdatetime, response?.data);
     })
   };
 
@@ -78,12 +74,12 @@ export default function Full () {
     // 날짜별로 데이터를 그룹화
     const dataEntries = Object.entries(data);
     const formattedData = dataEntries.reduce(
-    (acc: Array<{ title: number; date: string }>, [, value]) => {
+    (acc: Array<{ title: number; date: string; _id: string }>, [, value]) => {
     if (Array.isArray(value)) {
         const subAcc = value.reduce(
           (
-            subAcc: Array<{ title: number; date: string }>,
-            cur: { amount: number; date: string }
+            subAcc: Array<{ title: number; date: string; _id: string; }>,
+            cur: { amount: number; date: string; _id: string; }
           ) => {
                 const parsedDate = new Date(cur.date);
                 const dateKey = parsedDate.toISOString().split('T')[0];
@@ -98,13 +94,13 @@ export default function Full () {
                   if (foundPositive) {
                       foundPositive.title += cur.amount;
                   } else {
-                      subAcc.push({ title: cur.amount, date: dateKey });
+                      subAcc.push({ title: cur.amount, date: dateKey, _id: cur._id, });
                   }
                 } else {
                   if (foundNegative) {
                       foundNegative.title += cur.amount;
                   } else {
-                      subAcc.push({ title: cur.amount, date: dateKey });
+                      subAcc.push({ title: cur.amount, date: dateKey, _id: cur._id, });
                   }
                 }
                 return subAcc;
@@ -130,11 +126,6 @@ export default function Full () {
     Search();   
   },[]);
 
-  useEffect(() => {
-    handleDateClick(null);
-  },[viewData]);
-  
-
   const handlePlus = () => {
     setShowModal(true);
   }
@@ -146,7 +137,7 @@ export default function Full () {
   const getData = async(year: string,month: string) => {
     try {
       const response = await axios.get(
-        ` http://52.78.195.183:3003/api/expenses/calendar?year=${year}&month=${month}&userId=team10`
+        `http://52.78.195.183:3003/api/expenses/calendar?year=${year}&month=${month}&userId=team10`
       );
       console.log(response.data);
       return response;
@@ -160,16 +151,90 @@ export default function Full () {
     setViewDetail([]);
   }
 
-  const handleDateClick = async (info:any) => {
-    let time 
-    if(info){
-      time = new Date(info.event.startStr);
-    }else{
+  const handleDateClick = async (info: any, param?: any) => {
+    let paramdate = info;
+    if(info?.event?.startStr){
+      paramdate = info.event.startStr
+    }
+    let time;
+    if (info) {
+      time = new Date(paramdate);
+    } else {
       time = new Date();
     }
-    const day = time.getDate();
-    setViewDetail(viewData[0][day]);
-    console.log(viewDetail)
+    const day: number = time.getDate();
+    setSelectDate(time);
+    if(param){
+      setViewDetail(param[day]);
+    }else{
+      setViewDetail(viewData[0][day]);
+    }
+    
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => { 
+      setEditedTitle(e.target.value);
+  };
+
+  const handleEditButtonClick = (itemId: string) => {
+    if (confirm("수정하시겠습니까? 😮")) {
+      const itemToEdit = viewDetail.find((item) => item._id === itemId);
+      if (itemToEdit) {
+        setEditedTitle(`${Math.abs(itemToEdit.amount)}`);
+        setEditingItemId(itemId);
+      } else {
+        console.error("Item not found");
+      }
+    } else {
+      alert("취소되었습니다.");
+    }
+  };
+
+  const handleOutEditEnter = async (itemId: any) => {
+    if (!editedTitle || editedTitle.trim() === "") {
+      setEditingItemId('');
+    } else {
+      try {
+      let modifiedAmount = editedTitle;
+      if (editedTitle.startsWith('-')) {
+        modifiedAmount = editedTitle.slice(1); // 음수 기호를 제거하여 숫자 값만 전송
+      } else {
+        modifiedAmount = `-${editedTitle}`; // 양수일 경우 음수로 변경하여 전송
+      }
+      const response = await axios.put(`http://52.78.195.183:3003/api/expenses/${itemId}`, {
+        amount: modifiedAmount, // 수정된 값 전송
+      });
+      if (response.status === 200) {
+        alert("수정되었습니다.")
+        setEditingItemId('');
+        Search();
+      }
+    } catch (error) {
+      alert("에러 발생");
+      console.log(error);
+    }
+    }
+  };
+
+  const handleEditEnter = async (itemId: any) => {
+    if (!editedTitle || editedTitle.trim() === "") {
+      setEditingItemId('');
+    } else {
+      try {
+        setEditedTitle(editedTitle);
+        const response = await axios.put(`http://52.78.195.183:3003/api/expenses/${itemId}`, {
+          amount: editedTitle, // 수정된 값 전송
+        });
+        if (response.status === 200) {
+          alert("수정되었습니다.")
+          setEditingItemId('');
+          Search();
+        }
+      } catch (error) {
+        alert("에러 발생");
+        console.log(error);
+      }
+    }
   };
 
   const onDeletelist = async (itemId:any) => {
@@ -178,7 +243,14 @@ export default function Full () {
         const response = await axios.delete(`http://52.78.195.183:3003/api/expenses/${itemId}`);
         if (response.status === 200) {
           alert('삭제되었습니다');
-          Search();
+          const updatedViewDetail = viewDetail.filter(
+            (item: ViewDetailType) => item._id !== itemId
+          );
+          setViewDetail(updatedViewDetail);
+          const updatedViewDrow = viewDrow.filter(
+            (item) => item._id !== itemId
+          );
+          setViewDrow(updatedViewDrow);
         } else {
           alert('삭제 실패');
         }
@@ -211,39 +283,85 @@ export default function Full () {
           {viewDetail &&
             viewDetail.map((item) =>
               item.amount > 0 ? (
-                <div className="innerText">
-                  <InnerList>
-                  {item.amount}
-                    <span>
-                      <BsTrash className="trashIcon" 
-                        onClick={() => onDeletelist(item._id)}
+                <InnerList key={item._id}>
+                  <div className="innerText">
+                    {editingItemId === item._id ? (
+                      <input
+                        type="text"
+                        value={editedTitle}
+                        onChange={(e) => handleInputChange(e)}
+                        autoFocus
                       />
-                    </span>
-                  </InnerList>
-                </div>
+                    ) : (
+                      <>
+                        <div>{Math.abs(item.amount)}
+                          <BsPencil
+                            className="penIcon"
+                            onClick={() => handleEditButtonClick(item._id)}
+                          />
+                        </div>
+                      </>
+                    )}
+                    {editingItemId === item._id && (
+                      <div className="editControls">
+                        <BsCheckSquare
+                          className="checkIcon"
+                          onClick={() => handleEditEnter(item._id)}
+                        />
+                      </div>
+                    )}
+                    <BsTrash
+                      className="trashIcon"
+                      onClick={() => onDeletelist(item._id)}
+                    />
+                  </div>
+                </InnerList>
               ) : null
-            )
-          }
+            )}
         </ListboxLeft>
+
 
         <MinusMoney>지출</MinusMoney>
         <ListboxRight>
-          {viewDetail &&
-            viewDetail.map((item) => 
-              item.amount < 0 ? ( 
+        {viewDetail &&
+          viewDetail.map((item) => (
+            item.amount < 0 ? (
+              <InnerList key={item._id}>
                 <div className="innerText">
-                  <InnerList>
-                  {item.amount}
-                    <span>
-                      <BsTrash className="trashIcon"
-                      onClick={() => onDeletelist(item._id)}/>
-                    </span>
-                  </InnerList>
+                  {editingItemId === item._id ? (
+                    <input
+                      type="text"
+                      value={editedTitle}
+                      onChange={(e) => handleInputChange(e)}
+                      autoFocus
+                    />
+                  ) : (
+                    <>
+                      <div>{Math.abs(item.amount)}
+                        <BsPencil
+                          className="penIcon"
+                          onClick={() => handleEditButtonClick(item._id)}
+                        />
+                      </div>
+                    </>  
+                  )}
+                  {editingItemId === item._id && (
+                    <div className="editControls">
+                      <BsCheckSquare className="checkIcon"
+                      onClick={() => handleOutEditEnter(item._id)} />
+                    </div>
+                  )}
+                  <BsTrash
+                    className="trashIcon"
+                    onClick={() => onDeletelist(item._id)}
+                  />
                 </div>
-              ): null
-            )
-          }
+                
+              </InnerList>
+            ) : null
+          ))}
         </ListboxRight>
+
       </ListboxCenter>
 
       <PlusCirclreBox>
@@ -299,7 +417,6 @@ const CalendarBox = styled.div`
 
   .fc-event-title fc-sticky{
     padding: 2px;
- 
   }
 
   .fc-h-event{
@@ -379,10 +496,9 @@ const CalendarBox = styled.div`
 `
 const PlusCirclreBox = styled.div`
   width: 70px;
-  height: auto;
-  position: absolute;
-  top: 100px;
+  position: relative;
   left: 620px;
+  bottom: 20px;
 
   .plusbutton{
     font-size: 70px;
@@ -393,7 +509,6 @@ const PlusCirclreBox = styled.div`
 const ListDatabox = styled.div`
   width: 90%;
   position: relative;
-  height: 190px;
   top: 90px;
   left: 40px;
 `
@@ -402,6 +517,29 @@ const ListboxCenter = styled.div`
   position: relative;
   height:190px;
   margin: auto;
+
+  .innerText {
+    padding: 5px;
+    height: 28px;
+    position: relative;
+    border: none;
+    font-size: 15px;
+
+     input {
+      width: 70%;
+      background-color: #53c7a1;
+      color:#ffff;
+      font-size: 15px;
+      border: none;
+      outline: none;
+    } 
+
+    .editControls{
+      position: absolute;
+      top: 5px;
+      left: 170px;
+    }
+  }
 `
 const PlusMoneny = styled.div`
   width: 40px;
@@ -420,40 +558,53 @@ const MinusMoney = styled.div`
 
 const ListboxLeft = styled.div`
   width: 46%;
-  height: 180px;
+  padding-bottom: 230px;
   position: absolute;
   top: 25px;
   background-color: #53c7a1;
   float: left;
-  overflow-y: auto;
-  max-height: 160px;
+  overflow-y: 230px;
+  max-height: 200px;
   border-radius: 10px;
 `
 const InnerList = styled.div`
 width: 93%;
 padding: 5px;
-height: 30px;
+height: 35px;
 margin-top: 5px;
 margin-left: 10px;
-//background-color: yellow;
 border-bottom: 1px solid #F3EFE0;
 position: relative;
 color: white;
 
+.penIcon{
+  position: absolute;
+  left: 170px;
+}
+
 .trashIcon{
   position: absolute;
   left: 195px;
+  top: 6px;
 }
-
 `
 const ListboxRight = styled.div`
   width: 46%;
-  height: 180px;
+  height: 230px;
   position: relative;
   top: 25px;
   background-color: #FF6666;
   float: right;
-  overflow-y: auto;
-  max-height: 160px;
+  overflow-y: 230px;
+  max-height: 230px;
   border-radius: 10px;
+
+  input {
+    width: 70%;
+    background-color:#FF6666;
+    color:#ffff;
+    font-size: 15px;
+    border: none;
+    outline: none;
+  }
 `
