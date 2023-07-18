d79916de148a1e4d59ac54e711435bab99355a26
EungBug <eungb14@gmail.com>
2023-07-18 16:36:27 +0900

    fix: http load

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -3,7 +3,7 @@ import qs from 'query-string'
 
 axios.defaults.baseURL = 'http://52.78.195.183:3003/api/'
 
- export const api = axios
+export const api = axios
 
 export const apiInstance = axios.create()
 apiInstance.defaults.paramsSerializer = params => qs.stringify(params)
