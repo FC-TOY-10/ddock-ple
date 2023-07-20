049b9174210b228c3ebc6861bdcb0ecb9c396b2a
EungBug <eungb14@gmail.com>
2023-07-20 21:24:45 +0900

    refactor: 서버 URL 수정

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import qs from 'query-string'
 
-axios.defaults.baseURL = 'http://52.78.195.183:3003/api/'
+axios.defaults.baseURL = 'https://chickenlecture.xyz/api/'
 
 export const api = axios
 
